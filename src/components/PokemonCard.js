69e18ca64e4a4cef0f18cc053a25ffffe1035fd2
unknown <alexandre.alves@ucsal.edu.br>
2021-05-01 11:29:09 -0300

    Pokemon card first sketch

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -4,7 +4,24 @@ import './PokemonCard.css'
 export default function PokemonCard(props) {
     return (
         <div className="card-container">
-            <img src={props.pokemon.sprites.front_default} />
+            <div className="card-background">
+                <div className="pokemon-name">
+                    <span>{props.pokemon.name}</span>
+                    <span>#001</span>
+                </div>
+                <div className="pokemon-info">
+                    <div className="pokemon-types">
+                        {props.pokemon.types.map((item, index) => {
+                            return (
+                                <span>{item.type.name}</span>
+                            )
+                        })}
+                    </div>
+                    <img src={props.pokemon.sprites.front_default}/>
+                </div>
+            </div>
+
+            {/* <img src={props.pokemon.sprites.front_default} />
             <h1 className="pokemon-name">{props.pokemon.name}</h1>
             <div className="pokemon-types">
                 {props.pokemon.types.map((item, index) => {
@@ -12,7 +29,7 @@ export default function PokemonCard(props) {
                         <h1 className="pokemon-type" key={index}>{item.type.name}</h1>
                     )
                 })}
-            </div>
+            </div> */}
         </div>
     )
 }
